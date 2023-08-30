48abbf64143e6a64e2c9bb6dece38a257386ac7b
Pip <92968130+codepip55@users.noreply.github.com>
2023-08-30 20:13:06 +0200

    Mobile home design

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DeviceType } from 'src/app/models/deviceType';
+import { UserAgentService } from 'src/app/services/user-agent.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor (
+    private userAgentService: UserAgentService
+  ) { }
+
+  type: DeviceType;
+
+  ngOnInit(): void {
+    this.type = this.userAgentService.getDeviceType()
+  }
 
 }

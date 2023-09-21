1663a50447d16aeb82a3ea57a450c1773bbefbff
Pip <92968130+codepip55@users.noreply.github.com>
2023-09-21 20:04:43 +0200

    Format code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,42 +4,42 @@ import { firstValueFrom } from 'rxjs';
 import { StrapiService } from './services/strapi.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+	constructor(private strapiService: StrapiService) {}
 
-  constructor(
-    private strapiService: StrapiService,
-  ) { }
+	title = "Pepijn's Portfolio";
 
-  title = 'Pepijn\'s Portfolio';
+	loading: boolean = false;
 
-  loading: boolean = false;
+	async ngOnInit(): Promise<void> {
+		this.loading = true;
 
-  async ngOnInit(): Promise<void> {
-    this.loading = true;
+		if (new Date(window.localStorage.getItem('expiry')!) < new Date()) {
+			window.localStorage.removeItem('homeData');
+			window.localStorage.removeItem('languageData');
+			window.localStorage.removeItem('expiry');
+		}
 
-    if (new Date(window.localStorage.getItem('expiry')!) < new Date()) {
-      window.localStorage.removeItem('homeData')
-      window.localStorage.removeItem('languageData')
-      window.localStorage.removeItem('expiry')
-    }
+		if (
+			window.localStorage.getItem('homeData') &&
+			window.localStorage.getItem('languageData')
+		) {
+			this.loading = false;
+			return;
+		}
 
-    if (window.localStorage.getItem('homeData') && window.localStorage.getItem('languageData')) {
-      this.loading = false;
-      return;
-    }
+		let home = await firstValueFrom(this.strapiService.getHome());
+		let languages = await firstValueFrom(this.strapiService.getLanguages());
+		let expiry = new Date(new Date().getTime() + 60 * 60 * 24 * 3 * 1000); // 72 hours
 
-    let home = await firstValueFrom(this.strapiService.getHome())
-    let languages = await firstValueFrom(this.strapiService.getLanguages())
-    let expiry = new Date(new Date().getTime() + (60 * 60 * 24 * 3 * 1000)) // 72 hours
+		window.localStorage.setItem('homeData', JSON.stringify(home));
+		window.localStorage.setItem('languageData', JSON.stringify(languages));
+		window.localStorage.setItem('expiry', expiry.toString());
 
-    window.localStorage.setItem('homeData', JSON.stringify(home))
-    window.localStorage.setItem('languageData', JSON.stringify(languages))
-    window.localStorage.setItem('expiry', expiry.toString())
-
-    this.loading = false;
-  }
+		this.loading = false;
+	}
 }

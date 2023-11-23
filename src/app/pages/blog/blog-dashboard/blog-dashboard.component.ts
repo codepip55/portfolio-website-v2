de87942f05f1488085f235c63b2df5e8a5839702
Pip <92968130+codepip55@users.noreply.github.com>
2023-11-23 16:53:03 +0100

    Add blog components

---
+++ src/app/pages/blog/blog-dashboard/blog-dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-blog-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './blog-dashboard.component.html',
+  styleUrl: './blog-dashboard.component.scss'
+})
+export class BlogDashboardComponent {
+
+}
---
+++ src/app/pages/blog/blog-page/blog-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-blog-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './blog-page.component.html',
+  styleUrl: './blog-page.component.scss'
+})
+export class BlogPageComponent {
+
+}

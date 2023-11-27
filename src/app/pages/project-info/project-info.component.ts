8b3cae241276dba5b83a696e754f8324b6cb437d
Pip <92968130+codepip55@users.noreply.github.com>
2023-11-27 13:21:41 +0000

    Add project pages

--- src/app/pages/project-info/project-info.component.ts
+++ src/app/pages/project-info/project-info.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StrapiService } from 'src/app/services/strapi.service';
+import { lastValueFrom } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SeoService } from 'src/app/services/seo.service';
 
 @Component({
   selector: 'app-project-info',
@@ -8,6 +12,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './project-info.component.html',
   styleUrl: './project-info.component.scss'
 })
-export class ProjectInfoComponent {
+export class ProjectInfoComponent implements OnInit {
+
+  constructor(private strapiService: StrapiService, private activatedRoute: ActivatedRoute, private seoService: SeoService) {}
+
+  public project;
+
+  ngOnInit(): void {
+    // @ts-ignore
+    this.getBlog(this.activatedRoute.snapshot.paramMap.get('id'))
+    this.seoService.generateTags(this.project.data.attributes.title, this.project.data.attributes.description, this.project.data.attributes.cover_image)
+  }
+
+  private async getProject(id: string) {
+    let project = this.strapiService.getBlogs(id)
+    // @ts-ignore
+    project = await lastValueFrom(project)
+    this.project = project;
+    console.log(project)
+  }
 
 }

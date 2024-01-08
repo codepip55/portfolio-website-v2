845847bc209a992ed9bec601f7be4c70d0af578e
Pip <92968130+codepip55@users.noreply.github.com>
2024-01-08 13:43:20 +0000

    Add loader

--- src/app/pages/blog/blog-dashboard/blog-dashboard.component.ts
+++ src/app/pages/blog/blog-dashboard/blog-dashboard.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { StrapiService } from 'src/app/services/strapi.service';
 import { Router } from '@angular/router';
 import { StripHtmlPipe } from 'src/app/pipes/strip-html.pipe';
+import { LoaderComponent } from 'src/app/components/loader/loader.component';
 
 @Component({
 	selector: 'app-blog-dashboard',
 	standalone: true,
-	imports: [CommonModule, StripHtmlPipe],
+	imports: [CommonModule, StripHtmlPipe, LoaderComponent],
 	templateUrl: './blog-dashboard.component.html',
 	styleUrl: './blog-dashboard.component.scss',
 })
@@ -18,12 +19,17 @@ export class BlogDashboardComponent implements OnInit {
 	) {}
 
 	public blogPosts: any = [];
+	public loading: boolean = false;
 
 	ngOnInit(): void {
+		this.loading = true;
+
 		this.strapiService.getBlogs().subscribe((b: any) => {
 			this.blogPosts = b.data;
 			console.log(b.data);
 		});
+
+		this.loading = false;
 	}
 
 	public navigateToBlog(id: string) {

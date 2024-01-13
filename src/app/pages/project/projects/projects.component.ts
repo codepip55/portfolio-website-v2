2321f961d61203a10e944aacf43a596f76639795
Pip <92968130+codepip55@users.noreply.github.com>
2024-01-13 11:23:12 +0100

    Project pages

--- src/app/pages/project/projects/projects.component.ts
+++ src/app/pages/project/projects/projects.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StrapiService } from 'src/app/services/strapi.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-projects',
@@ -8,4 +10,33 @@ import { CommonModule } from '@angular/common';
 	templateUrl: './projects.component.html',
 	styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent {}
+export class ProjectsComponent implements OnInit {
+	constructor(
+		private strapiService: StrapiService,
+		private router: Router,
+	) {}
+
+	public projects: any[] = [];
+	public loading: boolean = false;
+
+	ngOnInit(): void {
+		this.loading = true;
+
+		this.strapiService.getProjects().subscribe((projects: any) => {
+			let unsortedProjects = projects.data;
+			let sortedProjects = unsortedProjects.sort(
+				(a, b) =>
+					new Date(b.attributes.publishedAt).getTime() -
+					new Date(a.attributes.publishedAt).getTime(),
+			);
+			this.projects = sortedProjects;
+			console.log(sortedProjects);
+		});
+
+		this.loading = false;
+	}
+
+	public navigateToProject(id: string) {
+		this.router.navigate(['/project/' + id]);
+	}
+}
